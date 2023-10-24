ef557f0ea87b77278f6f8c3913cab499cbeaeeb3
Sani <71819991+dzikrullah04@users.noreply.github.com>
2023-10-24 11:44:18 +0700

    kuis siapa paling manis sedunia

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 document.getElementById("redirectForm").addEventListener("submit", function(event) {
     event.preventDefault();
     const answer = document.getElementById("answer").value.trim().toLowerCase();
-    // reaksi adp + p akan menghasilkan?
-    if (answer === "a"  ) {
-        window.location.href = "https://families.google.com/jp/WTLz1qLDZEkSfKYwWV7-N2PXoYr6IQ";
-    } 
-    // reaksi nadp + h akan menghasilkan?
-     else if (answer === "j"  ){
-        window.location.href = " https://families.google.com/jp/0eGj-ZNBKhbfzK0_mkNxhbcIENXG4Q";
-    } 
-    // apakah FOTOFOSFORILASI non siklik melibatkan P680?
-    else if (answer === "7" || answer === 7 ){
-        window.location.href = "https://families.google.com/jp/TZQUkVF6Fh1K7iu5E4JjaDzYs8hxrA";
-    } else if (answer === "gita mutia puspita"){
-        window.location.href = " https://families.google.com/jp/RUt5cKmppEx8KP-XuSHTjey5tBv5yQ";
-    }else {
+    if (answer === "zahra" || answer === "jara" ) {
+        window.location.href = "https://families.google.com/jp/GO_s1ZAO6lnX3Gw9pNO4wUtGH0qHng";
+    } else {
         alert("Jawaban salah. Coba lagi!");
     }
 });
